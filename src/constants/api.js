ad0b8f314cb12c29c5268233d7949c8531a7eb20
mikaelrev <mikael.myrvang@gmail.com>
2022-11-20 15:18:13 +0100

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,1 @@
 export const BASE_URL = "https://api.noroff.dev/api/v1/";
-
-export const options = {
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDYwLCJuYW1lIjoiTWlrYWVsIiwiZW1haWwiOiJNaWtNeXIyOTA5NkBzdHVkLm5vcm9mZi5ubyIsImF2YXRhciI6IiIsImJhbm5lciI6IiIsImlhdCI6MTY2ODg2NjE0OX0.sTvcFCl899VJElXt5EtGiP8YIPjARGV_emTiOATgQmo",
-  },
-};
